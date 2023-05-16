4172361a4fc4584370f096c3f37b84a3b1fb668e
OH SUN CHUL <ispo3695@naver.com>
2023-05-16 18:15:15 +0900

    Implement Set_Direction() Method

--- test.cpp
+++ test.cpp
@@ -67,9 +67,9 @@ class GPIO
   private :
 
     
-  string default_path = "/sys/class/gpio/";
-  string export_file = default_path + "export"; 
-  string target_directory; 
+  string Common_Path = "/sys/class/gpio/";
+  string Export_File = Common_Path + "export"; 
+  string GPIO_Directory; 
   string Direction_File;
   string Value_File;
   
@@ -77,11 +77,14 @@ class GPIO
   public :
     
         GPIO(string GPIO_Number);
+       //  ~GPIO(); 
 
-        void Set_Value(int value);
+        int  Set_Value(string value);
         int  Get_Value();
         int Set_Direction(string Direction);
-        int Get_Direction();
+        // Get_Direction();
+
+        
 
 
 
@@ -96,40 +99,43 @@ GPIO::GPIO(string GPIO_Number)
 
    // export
 
-   int fd;
-   int status;
+   cout << "The Producer of this Progrma is OH SUN CHUL " << endl; 
+   cout << "Let's Constructiong gpio" << GPIO_Number << endl; 
 
-   fd = open(export_file.c_str(),O_WRONLY | O_CLOEXEC );
+   int fd;
+   GPIO_Directory = Common_Path + "gpio" + GPIO_Number +"/"; 
+  
+
+   fd = open(Export_File.c_str(),O_WRONLY | O_CLOEXEC );
 
    if( fd == -1 ) 
    {
-        cout << "Error to Open a File" << export_file << "....!!!!" << endl; 
-        cout << "Quit the Program" << endl; 
-        exit(0);
 
-   }
+    cout << "Error to Open a File" << Export_File << "...!!!" << endl; 
 
+
+   }  
    else
    {
      
-     cout << "파일을 여는데 성공하였습니다 ---> " << export_file << "...!!!" << endl; 
+     int status; 
+     cout << "Succeed to Open File : " << Export_File << "...!!!" << endl; 
      status = write(fd,GPIO_Number.c_str(),GPIO_Number.length());
 
-     if(status == -1 )
+     if(status == -1 ) 
      {
 
-        cout << export_file << "에 " << GPIO_Number << "를 쓰는데 실패하였습니다." << endl;
-        close(fd);
-        exit(0);
-
+      cout << "Error to Write " << GPIO_Number << " on File " << Export_File << endl;
 
      }
+                    
      else 
      {
         
-        target_directory = default_path + "gpio" + GPIO_Number +"/"; 
-        cout << export_file << "에 " << GPIO_Number << "를 쓰는데 성공하였습니다." << endl;
-        cout << target_directory << "가 생성되었습니다." << endl; 
+     
+        cout << "Succeed to Create " << GPIO_Directory << "...!!!" << endl; 
+        close(fd);
+             
 
      }
 
@@ -143,30 +149,99 @@ GPIO::GPIO(string GPIO_Number)
 int GPIO::Set_Direction(string Direction)
 {
 
-
-       if(Direction != "out" || Direction != "in")
-       {
-        cout << "방향은 out또는 in여야합니다." << endl; 
-        return -1;
-       }
-       else 
+  
+       if(Direction == "out" || Direction == "in" )
        {    
             int fd; 
-            Direction_File = target_directory + "direction";
+            Direction_File = GPIO_Directory + "direction";
+            
+            cout << "You set " << " [ " << Direction << " ] " << " on " << Direction_File << "...!!!" << endl; 
+            
             fd=open(Direction_File.c_str(),O_RDWR | O_CLOEXEC);
 
+            if( fd == -1 ) 
+            {
 
+               cout << "Error to Open File : " << Direction_File << "...!!!" << endl;
+               return -1; 
+            
+            }
+            else
+            {
+               int status;
+               cout << "Succeed to Open File : " << Direction_File << "...!!!" << endl;
+               status = write(fd,Direction.c_str(),strlen(Direction.c_str())); 
+               if(status == -1 ) 
+               {
+                  cout << "Error to Write " << Direction << " On File " << Direction_File << "...!!!" << endl; 
+                  return -1;
+
+               }
+               else 
+               {
+
+                 cout << "Succeed to Write " << " [ " << Direction << " ] " <<  " On File " << Direction_File << "...!!!" << endl; 
+                 close(fd); 
+
+               }
+               
+
+            }
 
 
        }
+       else 
+       {
+            cout << "Error to Set GPIO Direction" << endl; 
+            cout << "Direction has to be either out or in" << endl;
+            return -1; 
 
-  
 
-       
+       }
+    
 
+}
+
+
+int GPIO::Set_Value(string value)
+{
+     
+     int fd; 
+     Value_File = GPIO_Directory + "value";
+    
+
+     if( value == "0" || value == "1")
+     {
+           
+         fd = open(Value_File.c_str(),O_RDWR | O_CLOEXEC);
+         if( fd == -1) 
+         {
+
+            cout << "Error to Open File " << Value_File << "...!!!" <<endl;
+            return -1; 
+
+         }
+         else
+         {
+
+
+
+         }
+
+
+     }
+     else
+     {
+
+
+
+
+     }
 
 
 }
+
+
 
 
 
@@ -179,8 +254,13 @@ int main( int argc, char* argv[])
    cout << "Author : OH SUN CHUL" << endl;
    cout << "E-mail : ispo3695@google.com" << endl;
    cout << "E-mail : ispo3695@naver.com" << endl; 
+   
+   
+    GPIO gpio = GPIO(argv[1]); 
+    gpio.Set_Direction(argv[2]);
+    gpio.Set_Value(argv[3]); 
 
-   GPIO gpio = GPIO(argv[1]);
+    
 
 
   // test0();
